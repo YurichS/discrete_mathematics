686827badf32d93d60185d0cc079734c3e858f8e
Yurii <yurasidor26@gmail.com>
2022-03-09 13:37:49 +0200

    secant method

---
+++ secant_method.py
@@ -0,0 +1,20 @@
+# e = 10**(-4)
+# x*x - cos(x) = 0 [0.7;0.8]
+
+from math import cos, sin
+
+
+def f(x):
+    return x * x - cos(x)
+
+
+def secant_method(x_n, x_f, e):
+    r = f(x_f)
+    x_0 = x_n - (f(x_n) * (x_n - x_f)) / (f(x_n) - r)
+    while abs(x_0 - x_n) > e:
+        x_n = x_0
+        x_0 = x_n - (f(x_n) * (x_n - x_f)) / (f(x_n) - r)
+    return x_0
+
+
+print(secant_method(0.8, 0.7, 0.0001))
