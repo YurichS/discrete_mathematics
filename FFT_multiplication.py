b33487ab069809dd1e3fd7f8c7929ba155f15761
Yurii <yurasidor26@gmail.com>
2022-11-07 19:18:46 +0200

    fix FFT mult bug

--- FFT_multiplication.py
+++ FFT_multiplication.py
@@ -2,6 +2,7 @@ number_1 = '891026734517689412'
 number_2 = '75418901962345677'
 base_pow = 4
 
+
 def eq_len(num, count):
     l = len(num)
     while l < count:
@@ -13,7 +14,11 @@ def eq_len(num, count):
 def to_list(number):
     if base_pow >= len(number):
         raise ValueError("Power base is bigger than number length")
-    number = '0' * (len(number) % base_pow) + number
+    l = len(number) % base_pow
+    if l == 1:
+        number = '0' * (base_pow - 1) + number
+    else:
+        number = '0' * l + number
     number_list = [int(number[i:i + base_pow]) for i in range(0, len(number), base_pow)]
     number_list.reverse()
 
@@ -25,15 +30,16 @@ def DFT_multiply(a, b):
     a = to_list(a)
     b = to_list(b)
     max_i = len(a) + len(b) - 2
-    c_i = dict(zip(list(range(max_i+1)), [[] for i in range(max_i+1)]))
+    c_i = dict(zip(list(range(max_i + 1)), [[] for i in range(max_i + 1)]))
     for i in range(len(a)):
         for j in range(len(a)):
-            c_i[i+j].append(a[i]*b[j])
+            c_i[i + j].append(a[i] * b[j])
 
     result = 0
-    for i in range(max_i+1):
-        result+= sum(c_i[i])*(10**(base_pow*i))
+    for i in range(max_i + 1):
+        result += sum(c_i[i]) * (10 ** (base_pow * i))
 
     return result
+
 
 print(DFT_multiply(number_1, number_2))
